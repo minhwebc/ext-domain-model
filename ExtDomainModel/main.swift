77f497b888d66de15117e136dd163b53f5daf048
Quan Nguyen <minhquan611996@gmail.com>
2017-04-11 23:03:12 -0700

    Initial Commit

---
+++ ExtDomainModel/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ExtDomainModel
+//
+//  Created by Quan Nguyen on 4/11/17.
+//  Copyright © 2017 Quan Nguyen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
